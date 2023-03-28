117609eff9e3b8e60741caf93e3ffa61ae4cbaec
snoryder8019 <snoryder8019@gmail.com>
2023-03-28 09:17:06 -0600

    added notes to code

--- routes/auth/accounts.js
+++ routes/auth/accounts.js
@@ -8,7 +8,7 @@ const multer = require('multer');
 const upload =multer({dest:"uploads/"});
 const ObjectId = require('mongodb').ObjectId;
 const config = require('../../config/config')
-const Joi = require('joi')
+
 //////////////////middleware
 function isAddy(req,res,next){
   if(!req.user){res.redirect('login')}
@@ -16,53 +16,76 @@ if(req.user.isAdmin==true){
   next()}
     else{res.sendStatus(401)}
   }
-  /////////////////////////////////  
-  // Define a function that takes a `collections` object and returns a request handler function
-  function getServicesHandler(collections,route) {
-    return async (req, res) => {
-      try {
-        const clientIp = req.headers['x-forwarded-for'] || req.ip;
-        console.log(clientIp);  
-        const data = {
-          subpath: config.COLLECTION_SUBPATH,
-          dbName: config.DB_NAME,
-          collections,
-        };  
-        const response = await axios.get(config.DB_URL + '/api/readManyD', { params: data });
-        console.log(response.data);
-        res.render(route, { data: response.data });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-    };
-  }  
-  // Define the collections object
-  const svcCollections = {
-    0: '_services',
+  /////////////////~~~~~~~~~~~~~~~~~~~////////////////  
+  /////////////////////ACCOUNTS.JS//////////////////
+  //////////////~~~~~~~~~~~~~~~~~~~~~~~////////////////
+
+
+  
+  
+////////////////////////// MAIN .find() DYNAMIC FUNCTION /////////////////////////
+/////////////////////////////////////////////////////////
+///////GET FROM HTTPS://MONGO.W2MARKETING.BIZ////////////////
+function getHandler(collections,route) {
+  return async (req, res) => {
+    try {
+      const clientIp = req.headers['x-forwarded-for'] || req.ip;
+      console.log(clientIp);  
+      const data = {
+        subpath: config.COLLECTION_SUBPATH,
+        dbName: config.DB_NAME,
+        collections,
+      };  
+      const response = await axios.get(config.DB_URL + '/api/readManyD', { params: data });
+      console.log(response.data);
+      res.render(route, { data: response.data });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   };
+}  
+
+
+
+
+//////////////////////////////
+/////////////HANDLER ARGUMENTS/////////////////
+//////////////////////////////
+
+/////////////SERVICES.EJS/////////////////
+const svcCollections = {
+  0: '_services',
+};
+const servicesHandler = getHandler(svcCollections,'services');
+router.get('/services', servicesHandler);
+
+////////////ACCOUNTS.EJS//////////////////
   const clientsCollections = {
     0: '_clients',
   };  
-  // Define the request handler using the `getServicesHandler` function
-  const servicesHandler = getServicesHandler(svcCollections,'services');
-  const clientsHandler = getServicesHandler(clientsCollections,'accounts');
-  // Map the `/services` route to the `servicesHandler` function
-  router.get('/services', servicesHandler);
+  const clientsHandler = getHandler(clientsCollections,'accounts');
   router.get('/accounts', clientsHandler);
-  ///////////////////////////////
-// Define a function that takes the `ext` and `options` parameters and returns a request handler function
-function postToHandler(ext, options, route) {
-  return async (req, res) => {
-    const postData = {dbName: config.DB_NAME,subpath: config.COLLECTION_SUBPATH,ext, options};
-    try {
-      const response = await axios.post(config.DB_URL + '/publish/postToDb', postData);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-    res.redirect(route);
-  };
-}
+  
+  
+  
+  /////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
+  //////////// MAIN insertOne.() DYNAMIC FUNCTION ///////////////
+  ////////////POST TO HTTPS://MONGO.W2MARKETING.BIZ////////////////
+  // Define a function that takes the `ext` and `options` parameters and returns a request handler function
+  function postToHandler(ext, options, route) {
+    return async (req, res) => {
+      const postData = {dbName: config.DB_NAME,subpath: config.COLLECTION_SUBPATH,ext, options};
+      try {
+        const response = await axios.post(config.DB_URL + '/publish/postToDb', postData);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+      res.redirect(route);
+    };
+  }
 
 // Define the request handler using the `postToClientsHandler` function
 const postToClients = (req, res) => {
@@ -86,68 +109,10 @@ router.post('/postToClients', postToClients);
 router.post('/postToServices', postToServices);
 
 
+  /////////////////////////////////  
+  ///////////////ACCOUNTS.JS//////////////////
+  //////////////////////////////
 
-
-///////////////////////////////
-router.post('/postToClients0',(req, res)=> {
-  console.log(req.body)
-const postData = {
-dbName:config.DB_NAME,
-subpath:config.COLLECTION_SUBPATH,
-ext:req.body.collectionExtName ,
-options:{
-  contactName:req.body.contactName,
-  email:req.body.email,
-  companyName:req.body.companyName,
-  phoneNumber:req.body.phoneNumber,
-  businessAddress:req.body.businessAddress,
-  status:{
-    active:true,
-    delinquent:false,
-    invoice_list:"",
-    balance:""
-  }
-},validate:true
-  // Add any other data you want to send in the POST request
-};
-const postRequest = async () => {
-  try {
-    const response = await axios.post(config.DB_URL+'/publish/postToDb', postData);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-postRequest();
-res.redirect('accounts')
- })
-router.post('/postToServices0',(req, res)=> {
-  console.log(req.body)
-const postData = {
-dbName:config.DB_NAME,
-subpath:config.COLLECTION_SUBPATH,
-ext:req.body.collectionExtName ,
-options:{
-  serviceName:req.body.serviceName,
-  cost:req.body.cost,
-  terms:req.body.terms,
-  serviceCategory:req.body.serviceCategory,
-  serviceDetail:req.body.serviceDetail
-},
-validate:false
-  // Add any other data you want to send in the POST request
-};
-const postRequest = async () => {
-  try {
-    const response = await axios.post(config.DB_URL+'/publish/postToDb', postData);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-postRequest();
-res.redirect('services')
- })
 
  router.post('/removeService',(req, res)=> {
   console.log(req.body)
@@ -171,12 +136,7 @@ const postRequest = async () => {
 postRequest();
 res.redirect('services')
  })
-
-
-
-
-
-
-
-
+  /////////////////////////////////  
+  ///////////////ACCOUNTS.JS//////////////////
+  //////////////////////////////
 module.exports = router
