8af9684fa7fe94d6203eaa2974c9577265f1261f
snoryder8019 <snoryder8019@gmail.com>
2023-04-03 10:00:56 -0600

    final

--- routes/invoice/invoice.js
+++ routes/invoice/invoice.js
@@ -193,9 +193,10 @@ async function createUser(client,options,id){
           subject:`${config.COMPANY_NAME} ~ Invoice for: ${result1.companyName}`,        
           
           html:`<body style="margin:2%;padding:1%;text-align:center;background-color:black;color:white">
-          <h1><span>W2 Marketing ~ Invoice Availible</span></h1><br>
-          <img style="max-width:25%;transform:translateX(-75%);" src="cid:image1">
-          <h3>you can view your invoice at:</h3><br> <a href="https://app.w2marketing.biz/invoiceViewer/${req.url.split('/')[2]}">w2marketing.biz/invoiceViewer/${req.url.split('/')[2]}</a>
+          <h1>W2 Marketing ~ Invoice Availible</h1><br>
+          <img style="max-width:25%;transform:translateX(-75%);" src="cid:image1"><br>
+          <h3>you can view your invoice at:</h3><br> 
+          <a href="https://app.w2marketing.biz/invoiceViewer/${req.url.split('/')[2]}">w2marketing.biz/invoiceViewer/${req.url.split('/')[2]}</a><br>
           <p>Dear ${result1.contactName},</p>
           <p>Your invoice this month looks different because we have implemented a new invoicing feature within our web applications. This is an example of the power and flexibility of our web applications and the benefits they offer to businesses like  ${result1.companyName}.</p>
           <p>If you have any questions about the charges listed or any other aspect of our web app services, please do not hesitate to reach out to me directly at ${config.EMAIL}.</p>
