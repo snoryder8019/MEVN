bb5a9fe9c18a79df3992e66883b5577732e80a55
snoryder8019 <snoryder8019@gmail.com>
2023-04-03 16:07:04 -0600

    config update

---
+++ config/config_w2.js
@@ -0,0 +1,17 @@
+const config = {
+    PORT : '8282',
+    EMAIL:'w2marketing.scott@gmail.com',
+    DB_URL:'http://localhost:80',
+    DB_URL0:'https://mongo.w2marketing.biz',
+    DB_NAME: 'w2Apps',
+    IMAGE_FP: 'MEVN',
+    COMPANY_NAME:'W2 Marketing',
+    CONTACT_NAME:'Scott Wallace',
+    COMPANY_ADDRESS:'1536 14th AVE ~ Greeley, CO 80631',
+    COLLECTION_SUBPATH:'w2marketing',
+    BLOG:true,
+    ECOMMERCE:true,
+    OAUTH:true,  
+    ENV:'DEV'  
+}
+module.exports = config
