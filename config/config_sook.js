829b8df8090c596b74ce226dabc8a863e2e477b0
snoryder8019 <snoryder8019@gmail.com>
2023-04-04 09:46:41 -0600

    fixed pot issue

--- config/config_sook.js
+++ config/config_sook.js
@@ -1,7 +1,7 @@
 const config = {
     PORT : '8282',
     EMAIL:'w2marketing.scott@gmail.com',
-    DB_URL:'http://localhost:80',
+    DB_URL:'http://localhost:8009',
     DB_URL0:'https://mongo.w2marketing.biz',
     DB_NAME: 'w2Apps',
     IMAGE_FP: 'MEVN',
