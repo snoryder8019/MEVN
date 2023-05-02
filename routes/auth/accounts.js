fe61ede4ac3ef8da3cbc1af8c7e9ebfc9bf8074a
snoryder8019 <snoryder8019@gmail.com>
2023-05-02 08:44:34 -0600

    client dashboard link, editor fix  deleting status

--- routes/auth/accounts.js
+++ routes/auth/accounts.js
@@ -115,7 +115,7 @@ router.get('/editor/:?',isAddy, editorHandler);
 const postToClients = (req, res) => {
   const { collectionExtName, contactName, email, companyName, phoneNumber, businessAddress } = req.body;
   const options = {contactName,email,companyName,phoneNumber,businessAddress,
-    status: {active: true,delinquent: false,invoice_list: [],balance: 0}};    
+    status: {active: true,balance: 0}};    
     const handler = postToHandler(collectionExtName, options, 'accounts');
     handler(req, res);
   };
